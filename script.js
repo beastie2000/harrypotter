df5323859c311a03e22d764dce143526dad7fec6
Olivia Harrison <livharrison3@gmail.com>
2021-10-11 10:47:18 -0600

    olivia starting to incorporate api

---
+++ script.js
@@ -0,0 +1,15 @@
+// Olivia's JS for getting a random character
+document.getElementById("random-character-button").addEventListener("click", function(event) {
+  console.log("Clicked!");
+
+  const url = "http://hp-api.herokuapp.com/api/characters";
+  console.log(url);
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+      console.log(json.length);
+    //  document.getElementById("testing-button").innerHTML = "modified document";
+      });
+});
