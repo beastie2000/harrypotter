03780135e9f85021cd202c52e2a75e8a5350db09
beast2000 <mlpierce00@gmail.com>
2021-10-14 20:17:41 -0600

    almost finished

--- house-characters.js
+++ house-characters.js
@@ -1,12 +1,18 @@
 // Maura's JS for getting list of characters
 
 document.getElementById("houseSubmit").addEventListener("click", function(event) {
-    console.log("clicked");
     event.preventDefault();
     const value = document.getElementById("houseInput").value;
       if (value === "") 
         return;
-      console.log(value);
+      if (value === "Gryffindor" || value === "Slytherin" || value === "Hufflepuff" || value === "Ravenclaw" || value === "gryffindor" || value === "slytherin" || value === "hufflepuff" || value === "ravenclaw") {
+        console.log("valid house");
+      }
+      else {
+        alert("Please type a valid hogwarts house");
+        return;
+      }
+
       const url = "http://hp-api.herokuapp.com/api/characters/house/" + value;
       fetch(url)
         .then(function(response) {
@@ -15,16 +21,38 @@ document.getElementById("houseSubmit").addEventListener("click", function(event)
           console.log(json);
           let results = "";
           for (let i = 0; i < json.length; i++) {
-              results += "<p>" + json[i].name;
-              results += "<p>" + json[i].image;
-              results += "<p>" + json[i].actor;
-              results += "<p>" + json[i].dateOfBirth;
-              results += "<p>" + json[i].patronus;
-              results += "<p>" + json[i].species;
-              results += "<p>" + json[i].dateOfBirth;
-              results += "<p>" + json[i].dateOfBirth;
-              results += "<p>" + json[i].wand.core;
-              results += "<p>" + json[i].wand.wood;
+              results += "<h1 ";
+              if (json[i].house === "Slytherin") {
+                results += "id='slyth'";
+              } else if (json[i].house === "Ravenclaw") {
+                results += "id='raven'";
+              } else if (json[i].house === "Gryffindor") {
+                results += "id='gryff'";
+              } else if (json[i].house === "Hufflepuff") {
+                results += "id='huffl'";
+              } 
+              results += ">" + json[i].name + "</h1>";
+              results += "<img src='" + json[i].image + "' width='200'>";
+              if (json[i].species !== "human") {
+                results += "<p>" + json[i].species + "</p>";
+              }
+              else if (json[i].hogwartsStudent) {
+                results += "<p>" + json[i].gender + " Hogwarts Student</p>";
+              }
+              else if (json[i].hogwartsStaff) {
+                results += "<p>" + json[i].gender + " Hogwarts Staff</p>"
+              }
+              else if (json[i].wizard) {
+                results += "<p>" + json[i].gender + " Wizard</p>";
+              }
+              else if (json[i].ancestry === "squib") {
+                results += "<p>" + json[i].gender + " Squib</p>";
+              }
+
+              results += "<p>" + json[i].wand.length + " inch " + json[i].wand.wood + " wand with a " + json[i].wand.core + " core</p>";
+              results += "<p>Patronus: " + json[i].patronus + "</p>";
+              results += "<p>Birthday: " + json[i].dateOfBirth + "</p>";
+              results += "<p>Played by " + json[i].actor + "</p>";
           }
           results += "</p>";
           document.getElementById("houseResults").innerHTML = results;
